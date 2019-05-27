81729c8e15a958ae05174c598359520b7b684e95
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-05-27 22:12:18 +0100

    added time service to handle time operations

---
+++ src/app/services/time/time.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class TimeService {
+	private format: string;
+
+	setFormat(format: string): void {
+		this.format = format;
+	}
+
+	minutesToHours(minutesToConvert: number): string {
+		const hours = Math.floor(minutesToConvert / 60);
+		const minutes = minutesToConvert % 60;
+
+		return (hours > 0)
+			? `${hours}h${minutes}m`
+			: `${minutes}m`;
+	}
+
+	formatTime(d: Date): string {
+		let result = this.format;
+
+		result = result.replace("dd", this.addLeadZero(d.getDate()));
+		result = result.replace("mm", this.addLeadZero(d.getMonth() + 1));
+		result = result.replace("yyyy", `${d.getFullYear()}`);
+
+		return result;
+	}
+
+	addTime(d: Date, minutes: number, hours?: number): string {
+		if (hours) {
+			d.setHours(d.getHours() + hours);
+		}
+
+		d.setMinutes(d.getMinutes() + minutes);
+
+		return this.formatTime(d);
+	}
+
+	private addLeadZero(num: number): string {
+		return `0${num}`.substr(-2);
+	}
+}
