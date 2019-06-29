371e8478027621a757f3e0f9e4628ed2df35360f
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-06-29 19:06:43 +0100

    refactored extra / owed hours to own component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -14,12 +14,13 @@ import { HomePage } from './home.page';
 /* comps */
 import { ListItemComponent } from 'src/app/components/list-item/list-item.component';
 import { ListButtonComponent } from 'src/app/components/list-button/list-button.component';
+import { UpdateLunchModalComponent } from 'src/app/components/update-lunch-modal/update-lunch-modal.component';
+import { OwedExtraHoursComponent } from 'src/app/components/owed-extra-hours/owed-extra-hours.component';
 
 /* modules */
 import { ConvertTimeModule } from 'src/app/pipes/convert-time/convert-time.pipe.module';
 import { CalcClockoutModule } from 'src/app/pipes/calc-clockout/calc-clockout.pipe.module';
 import { FormatTimeModule } from 'src/app/pipes/format-time/format-time.pipe.module';
-import { UpdateLunchModalComponent } from 'src/app/components/update-lunch-modal/update-lunch-modal.component';
 
 import { SanitizerModule } from 'src/app/pipes/sanitizer/sanitizer.pipe.module';
 
@@ -30,7 +31,8 @@ export const createTranslateLoader = (http: HttpClient) => new TranslateHttpLoad
 		HomePage,
 		ListItemComponent,
 		ListButtonComponent,
-		UpdateLunchModalComponent
+		UpdateLunchModalComponent,
+		OwedExtraHoursComponent
 	],
 	imports: [
 		CommonModule,
