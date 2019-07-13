71f9759079ecc5a76440e6812d287d591dbe3548
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-07-13 19:30:47 +0100

    added loader comp

---
+++ src/app/components/loader/loader.component.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { LoaderComponent } from './loader.component';
+
+@NgModule({
+    declarations: [LoaderComponent],
+    exports: [LoaderComponent]
+})
+export class LoaderModule { }
