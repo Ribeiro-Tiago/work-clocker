8b3d5df1870a76737b1fac17c53caeaba9c323d4
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-07-14 16:14:48 +0100

    moved missing module imports

--- src/app/pages/clocked-hours/clocked-hours.module.ts
+++ src/app/pages/clocked-hours/clocked-hours.module.ts
@@ -5,15 +5,22 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ClockedHoursPage } from './clocked-hours.page';
 import { SharedModule } from 'src/app/shared.module';
 
+import { ListItemComponent } from 'src/app/components/list-item/list-item.component';
+import { CalcClockoutModule } from 'src/app/pipes/calc-clockout/calc-clockout.pipe.module';
+
 @NgModule({
 	imports: [
 		SharedModule,
 		TranslateModule,
+		CalcClockoutModule,
 		RouterModule.forChild([{
 			path: '',
 			component: ClockedHoursPage
 		}])
 	],
-	declarations: [ClockedHoursPage]
+	declarations: [
+		ClockedHoursPage,
+		ListItemComponent,
+	]
 })
 export class ClockedHoursPageModule { }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-/* pages */
 import { HomePage } from './home.page';
 
-/* comps */
-import { ListItemComponent } from 'src/app/components/list-item/list-item.component';
 import { UpdateLunchModalComponent } from 'src/app/components/update-lunch-modal/update-lunch-modal.component';
 import { OwedExtraHoursComponent } from 'src/app/components/owed-extra-hours/owed-extra-hours.component';
 
-/* modules */
-import { CalcClockoutModule } from 'src/app/pipes/calc-clockout/calc-clockout.pipe.module';
 import { SharedModule } from 'src/app/shared.module';
 
 @NgModule({
 	declarations: [
 		HomePage,
-		ListItemComponent,
 		UpdateLunchModalComponent,
 		OwedExtraHoursComponent,
 	],
 	imports: [
-		CalcClockoutModule,
 		SharedModule,
 		RouterModule.forChild([{
 			path: '',
