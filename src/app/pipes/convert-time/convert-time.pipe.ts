07f3f68b0208bead9db0d537c37fb947852f3bc6
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-07-29 23:30:20 +0100

    updated convert time pipeline to allow for sentences with multiple arguments ( needs refactor )

--- src/app/pipes/convert-time/convert-time.pipe.ts
+++ src/app/pipes/convert-time/convert-time.pipe.ts
@@ -7,26 +7,73 @@ interface ConvertTimeOpts {
 	format?: string;
 }
 
+interface PayloadOption {
+	key: string;
+	value: string;
+}
+
 @Pipe({
 	name: 'convertTime'
 })
 export class ConvertTimePipe implements PipeTransform {
 	constructor(private timeHandle: TimeService) { }
 
-	transform(value: string, { destType, payload, format }: ConvertTimeOpts): any {
-		let replaceText: string;
+	transform(originText: string, { destType, payload, format }: ConvertTimeOpts): any {
+		let replaceText: string | PayloadOption[];
+		const multipleFields = (typeof payload === "object");
 
-		if (destType === "day") {
-			this.timeHandle.setFormat(format);
-			replaceText = this.timeHandle.formatDate(new Date(payload));
-		} else if (destType === "hours") {
-			replaceText = this.timeHandle.minutesToHours(payload);
+		if (multipleFields) {
+			if (destType === "day") {
+				this.timeHandle.setFormat(format);
+
+				replaceText = Object.entries(payload).map(entry => ({
+					key: entry[0],
+					value: this.timeHandle.formatDate(new Date(entry[1] as number))
+				}));
+
+			} else if (destType === "hours") {
+				replaceText = Object.entries(payload).map(entry => ({
+					key: entry[0],
+					value: this.timeHandle.minutesToHours(entry[1] as number)
+				}));
+			} else {
+				let d: Date;
+
+				replaceText = Object.entries(payload).map(entry => {
+					d = new Date(entry[1] as number);
+
+					return {
+						key: entry[0],
+						value: this.timeHandle.minutesToHours(d.getHours() * 60 + d.getMinutes())
+					};
+				});
+			}
 		} else {
-			const d = new Date(payload);
+			if (destType === "day") {
+				this.timeHandle.setFormat(format);
+				replaceText = this.timeHandle.formatDate(new Date(payload));
+			} else if (destType === "hours") {
+				replaceText = this.timeHandle.minutesToHours(payload);
+			} else {
+				const d = new Date(payload);
 
-			replaceText = this.timeHandle.minutesToHours(d.getHours() * 60 + d.getMinutes());
+				replaceText = this.timeHandle.minutesToHours(d.getHours() * 60 + d.getMinutes());
+			}
 		}
 
-		return value.replace(/{{.*?}}/g, replaceText);
+		if (multipleFields) {
+			let text = originText;
+
+			console.log(text);
+			(replaceText as PayloadOption[])
+				.forEach(({ key, value }) => {
+					console.log(key, value);
+					text = text.replace(`{{ ${key} }}`, value);
+				});
+
+			return text;
+		}
+
+		return originText.replace(/{{.*?}}/g, replaceText as string);
 	}
 }
