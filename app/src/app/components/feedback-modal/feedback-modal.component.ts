6408da04e9009e39246aff519ee6afe4b4fbc477
Tiago RIBEIRO <tiagoribeiro@Administrators-MacBook-Pro.local>
2019-10-07 19:45:57 +0200

    - implemented openEmail function - ui tweak

--- app/src/app/components/feedback-modal/feedback-modal.component.ts
+++ app/src/app/components/feedback-modal/feedback-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter } from "@angular/core";
 import { AppVersion } from "@ionic-native/app-version/ngx";
 import { Device } from "@ionic-native/device/ngx";
+import { TranslateService } from "@ngx-translate/core";
 
 import configs from "./configs";
 
@@ -17,16 +18,33 @@ export class FeedbackModalComponent {
 	topic: FeedBackTopic;
 	topics: FeedBackTopic[];
 
-	constructor(private appVersion: AppVersion, private device: Device) {
+	constructor(private appVersion: AppVersion, private device: Device, private translate: TranslateService) {
 		this.topics = configs.topics;
+		this.topic = configs.topics[0];
 
 		this.onCancel = new EventEmitter();
 	}
 
-	openEmail(subject: string): void {
-		window.location.href = `mailto:${configs.feedbackEmail}?subject=${subject}&body=message%20goes%20here`;
-	}
+	async openEmail(): Promise<void> {
+		const texts = await Promise.all([
+			this.translate.get(this.topic.emailSubject).toPromise(),
+			this.translate.get("feedback.problem_body").toPromise(),
+		]);
+		let body = "";
 
+		if (this.topic.value === "problem") {
+			body = `
+				%0D%0A%0D%0A-------------------------
+				%0D%0A${texts[1]}%0D%0A
+				> App version: ${await this.appVersion.getVersionNumber()} / ${this.device.cordova}%0D%0A
+				> System: ${this.device.platform}%0D%0A
+				> System version: ${this.device.version}%0D%0A
+				> Model: ${this.device.model}
+			`;
+		}
+
+		window.location.href = `mailto:${configs.feedbackEmail}?subject=${texts[0]}&body=${body}`;
+	}
 	triggerCancel(): void {
 		this.onCancel.emit();
 	}
