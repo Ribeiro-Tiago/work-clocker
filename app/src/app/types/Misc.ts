da85e0a1f54d287999cb8725a732c93cda640641
Tiago RIBEIRO <tiagoribeiro@Administrators-MacBook-Pro.local>
2019-10-07 15:37:45 +0200

    - added feedback comp texts & typing

--- app/src/app/types/Misc.ts
+++ app/src/app/types/Misc.ts
@@ -1,36 +1,42 @@
 export interface ConfigOption {
-    label: string;
-    hour?: string;
-    key: string;
+	label: string;
+	hour?: string;
+	key: string;
 }
 
 export interface LegalOption {
-    labelId: string;
-    link?: string;
-    version?: string;
+	labelId: string;
+	link?: string;
+	version?: string;
 }
 
 export interface LangItem {
-    icon: string;
-    label: string;
-    key: string;
+	icon: string;
+	label: string;
+	key: string;
 }
 
 export interface NotifOption {
-    enabled: boolean;
-    time: string;
+	enabled: boolean;
+	time: string;
 }
 
 export interface GenericOption {
-    label: string;
-    value: string;
+	label: string;
+	value: string;
 }
 
 export interface OwedHourModalConf {
-    isVisible: boolean;
-    isExtra: boolean;
-    isPool: boolean;
-    owedHours: number;
-    extraHours?: number;
-    poolHours?: number;
+	isVisible: boolean;
+	isExtra: boolean;
+	isPool: boolean;
+	owedHours: number;
+	extraHours?: number;
+	poolHours?: number;
+}
+
+export interface FeedBackTopic {
+	label: string;
+	value: string;
+	emailSubject: string;
 }
