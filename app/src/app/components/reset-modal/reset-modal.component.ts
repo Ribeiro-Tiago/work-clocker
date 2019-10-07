c6c38f841d24f9f1ca0b9952610c6c2e483e916f
Tiago RIBEIRO <tiagoribeiro@Administrators-MacBook-Pro.local>
2019-10-07 15:48:22 +0200

    - tslint fixes

--- app/src/app/components/reset-modal/reset-modal.component.ts
+++ app/src/app/components/reset-modal/reset-modal.component.ts
@@ -1,9 +1,9 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
-	selector: 'app-reset-modal',
-	templateUrl: './reset-modal.component.html',
-	styleUrls: ['./reset-modal.component.scss'],
+	selector: "app-reset-modal",
+	templateUrl: "./reset-modal.component.html",
+	styleUrls: ["./reset-modal.component.scss"],
 })
 export class ResetModalComponent {
 	@Output() onCancel: EventEmitter<void>;
