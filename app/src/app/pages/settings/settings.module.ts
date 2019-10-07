081bc0bffd3702d8051fbd9b43ef3d5769ccb022
Tiago RIBEIRO <tiagoribeiro@Administrators-MacBook-Pro.local>
2019-10-07 10:39:42 +0200

    - removed unsued open file imports - added vibration native module

--- app/src/app/pages/settings/settings.module.ts
+++ app/src/app/pages/settings/settings.module.ts
@@ -1,30 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppVersion } from "@ionic-native/app-version/ngx";
+import { Vibration } from "@ionic-native/vibration/ngx";
 
-import { SettingsPage } from './settings.page';
+import { SettingsPage } from "./settings.page";
 
-import { ResetModalComponent } from 'src/app/components/reset-modal/reset-modal.component';
+import { ResetModalComponent } from "src/app/components/reset-modal/reset-modal.component";
 
-import { SharedModule } from 'src/app/shared.module';
-import { AppVersion } from '@ionic-native/app-version/ngx';
-import { File } from "@ionic-native/file/ngx";
-import { FileOpener } from "@ionic-native/file-opener/ngx";
-import { DocumentViewer } from "@ionic-native/document-Viewer/ngx";
+import { SharedModule } from "src/app/shared.module";
 
 @NgModule({
 	imports: [
 		SharedModule,
 		RouterModule.forChild([{
-			path: '',
+			path: "",
 			component: SettingsPage
 		}])
 	],
 	declarations: [SettingsPage, ResetModalComponent],
 	providers: [
 		AppVersion,
-		File,
-		FileOpener,
-		DocumentViewer
+		Vibration
 	]
 })
 export class SettingsPageModule { }
