741d7328888a59e541b6984ae5ce12a6c354e5af
Tiago RIBEIRO <tiagoribeiro@Administrators-MacBook-Pro.local>
2019-10-08 11:47:42 +0200

    - fixed linting issues - added analytics provider

--- app/src/app/shared.module.ts
+++ app/src/app/shared.module.ts
@@ -1,38 +1,44 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { HeaderModule } from './components/header/header.module';
-import { ConvertTimeModule } from './pipes/convert-time/convert-time.pipe.module';
-import { SanitizerModule } from './pipes/sanitizer/sanitizer.pipe.module';
-import { FormatTimeModule } from './pipes/format-time/format-time.pipe.module';
-import { TranslateModule } from '@ngx-translate/core';
-import { CalcClockoutModule } from './pipes/calc-clockout/calc-clockout.pipe.module';
+import { FirebaseAnalytics } from "@ionic-native/firebase-analytics/ngx";
+
+import { TranslateModule } from "@ngx-translate/core";
+
+import { HeaderModule } from "./components/header/header.module";
+import { ConvertTimeModule } from "./pipes/convert-time/convert-time.pipe.module";
+import { SanitizerModule } from "./pipes/sanitizer/sanitizer.pipe.module";
+import { FormatTimeModule } from "./pipes/format-time/format-time.pipe.module";
+import { CalcClockoutModule } from "./pipes/calc-clockout/calc-clockout.pipe.module";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        ConvertTimeModule,
-        HeaderModule,
-        FormatTimeModule,
-        SanitizerModule,
-        TranslateModule,
-        CalcClockoutModule
-    ],
-    exports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        ConvertTimeModule,
-        FormatTimeModule,
-        HeaderModule,
-        SanitizerModule,
-        TranslateModule,
-        CalcClockoutModule
-    ]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		ConvertTimeModule,
+		HeaderModule,
+		FormatTimeModule,
+		SanitizerModule,
+		TranslateModule,
+		CalcClockoutModule
+	],
+	exports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		ConvertTimeModule,
+		FormatTimeModule,
+		HeaderModule,
+		SanitizerModule,
+		TranslateModule,
+		CalcClockoutModule
+	],
+	providers: [
+		FirebaseAnalytics
+	]
 })
 export class SharedModule { }
